ae3a381d72907bad7a1b2b8dbede1ab1570f5c72
kensevv <kensev2000@gmail.com>
2020-12-27 15:35:38 +0200

    Base functions implementation

--- Project - Musical Collection/Project - Musical Collection/Project - Musical Collection.cpp
+++ Project - Musical Collection/Project - Musical Collection/Project - Musical Collection.cpp
@@ -4,7 +4,22 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include "Date.h"
+#include "Genre.h"
+#include "Song.h"
+#include "Playlist.h"
+#include "User.h"
+#include "UserInterface.h"
+
+std::vector<User> users;
+std::vector<Playlist> playlists;
+std::vector<Song> songs;
+
+void readFile();
+void login();
+//todo
 
 int main()
 {
+
 }
--- Project - Musical Collection/Project - Musical Collection/UserInterface.h
+++ Project - Musical Collection/Project - Musical Collection/UserInterface.h
@@ -11,10 +11,25 @@
 class UserInterface
 {
 private:
-	User user;
-	std::vector<Playlist> playlists;
-	std::vector<Song> songs;
+	User* user;
+	std::vector<Playlist>* playlists;
+	std::vector<Song>* songs;
+
+	Playlist* currentPlaylist;
 public:
 	UserInterface();
 	UserInterface(const User& user, const std::vector<Playlist>& playlists, const std::vector<Song>& songs);
+	
+	// rating song is done via class User
+	// adding/removing fav genre is done by class user
+	void addSong(); // add song to the database
+	void generatePlaylistForUser(); // criterias
+	void saveCurrentPlaylist(const std::string& name);
+	void loadPlaylistByName(const std::string& name);
+	void sortPlaylist(); // criteria
+	void showCurrPlaylistInfo(); // info for songs in current playlist
+
+	void loadData(); // not sure yet, may be done with constructor.
+	void updateDatabase(); // write updated global containers back to the file
+	void logout(); // logout from user and unload all data from the global containers
 };
